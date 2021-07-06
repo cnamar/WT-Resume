60146df217e91d08df8de3edf2e50eb9806a606e
cnamar <amarnathcn6112000@gmail.com>
2021-07-07 01:15:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+$("#service-section").hide();
+$("#skills-section").hide();
+$("#contact-section").hide();
+var pagestatus = [
+  { id: "#home-section", status: true },
+  { id: "#skills-section", status: false },
+  { id: "#service-section", status: false },
+  { id: "#contact-section", status: false },
+];
+
+showpage = (event) => {
+  var secid = "#" + event.srcElement.id + "-section";
+  $(secid).show();
+  for (i = 0; i < 4; i++) {
+    if (pagestatus[i].status) {
+      $(pagestatus[i].id).hide();
+      pagestatus[i].status = false;
+    }
+    if (pagestatus[i].id === secid) {
+      $(pagestatus[i].id).show();
+      pagestatus[i].status = true;
+    }
+  }
+};
+sub = () => {
+  alert("Your data will be submitted later.Now It's just a static web page.");
+};
